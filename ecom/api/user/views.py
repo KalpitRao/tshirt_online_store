485822b9a34f5011e032868494f52a3f8858108a
Kalpit <kalpitraorane46@gmail.com>
2022-05-20 01:42:38 -0700

    Initial commit

--- ecom/api/user/views.py
+++ ecom/api/user/views.py
@@ -1,3 +1,5 @@
+from ast import Try
+from urllib.parse import ParseResultBytes
 from rest_framework import viewsets
 from rest_framework.permission import AllowAny
 from .serializers import UserSerializer
@@ -14,3 +16,43 @@ import re
 def generate_session_token(length=10):
     return ''.join(random.SystemRandom().choice([chr(i) for i in range(97,123)] + [str(i) for i in range(10)])for _ in range(length)) #we are using this to generate random token run this command in python shell.first import random.
 
+@csrf_exempt
+def signin(request):
+    if not request.method == 'POST':
+        return JsonResponse({'error':'Send a post request with valid parameter only'})
+
+    username = request.POST['email']
+    password = request.POST['password']
+#validation part
+    if not re.match("([\w\.\-_]+)?\w+@[\w-_]+(\.\w+){1,}",username):
+        return JsonResponse({'error':'Enter a valid email'})
+
+    if len(password) < 3:
+        return JsonResponse({'error':'Password needs to be atleast of 3 char'})
+
+    UserModel= get_user_model()
+
+    try:
+        user = UserModel.objects.get(email=username)
+
+        if user.check_password(password):
+            usr_dict = UserModel.objects.filter(email=username).values().first()
+            usr_dict.pop('password')
+
+            if user.session_token != "0":
+                user.session_token = "0"
+                user.save()
+                return JsonResponse({'error':'Previous session exists!'})
+            
+            token = generate_session_token()
+            user.session_token = token
+            user.save()
+            login(request, user)
+            return JsonResponse({'token':token,'user':usr_dict})
+
+        else:
+            return JsonResponse({'error' : 'Invalid password'})
+
+        
+    except UserModel.DoesNotExist:
+        return JsonResponse({'error':'Invalid Email'})
