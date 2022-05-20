d72a94d4abbc87852d7c7a1d5f2d38974316399d
Kalpit <kalpitraorane46@gmail.com>
2022-05-20 01:02:19 -0700

    Initial commit

--- ecom/api/user/views.py
+++ ecom/api/user/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permission import AllowAny
+from .serializers import UserSerializer
+from .models import CustomUser
+from django.http import JsonResponse
+from django.contrib.auth import get_user_model
+from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth import login, logout
 
+import random
+import re
 # Create your views here.
+
+def generate_session_token(length=10):
+    return ''.join(random.SystemRandom().choice([chr(i) for i in range(97,123)] + [str(i) for i in range(10)])for _ in range(length)) #we are using this to generate random token run this command in python shell.first import random.
+
