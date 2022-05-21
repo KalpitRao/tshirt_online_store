68f49dc86f17c21693bf5a569709e5be0ed5ad66
Kalpit <kalpitraorane46@gmail.com>
2022-05-21 03:38:34 -0700

    Initial commit

--- ecom/api/user/views.py
+++ ecom/api/user/views.py
@@ -56,3 +56,32 @@ def signin(request):
         
     except UserModel.DoesNotExist:
         return JsonResponse({'error':'Invalid Email'})
+
+
+
+def signout(request, id):
+    logout(request)
+
+    UserModel = get_user_model
+
+    try:
+        user = UserModel.objects.get(pk=id)
+        user.session_token = '0'
+        user.save()
+    except UserModel.DoesNotExist:
+        return JsonResponse({'error':'Invalid user ID'})
+
+    return JsonResponse({'success':'Logout success'})
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    permission_classes_by_action = {'create': [AllowAny]}
+
+    queryset = CustomUser.objects.all().order_by('id')
+    serializer_class = UserSerializer
+
+    def get_permissions(self):
+        try:
+            return [permission() for permission in self.permission_classes_by_action[self.action]]
+        except KeyError:
+            return [permission() for permission in self.permission_classes]
