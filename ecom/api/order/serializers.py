dfe8855dc26bc4bce841d13652ddbba6dde5dd85
Kalpit <kalpitraorane46@gmail.com>
2022-05-28 01:39:18 -0700

    Initial commit

---
+++ ecom/api/order/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+
+from .models import Order
+
+class OrderSerializer(serializers.HyperlinkedModelsSerializer):
+    class Meta:
+        model = Order
+        fields  = ('user')
+        #TODO:add product and quantity
